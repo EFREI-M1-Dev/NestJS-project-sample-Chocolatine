10176e498afae9f6979fb85ab8a464976584c407
Chocolatiines <pimooshwoom@gmail.com>
2024-02-20 11:21:23 +0100

    add routes get/post

--- nestjs-project-sample/src/app.controller.ts
+++ nestjs-project-sample/src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {AppService} from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    // Route GET dotée d'un paramètre qui renvoie la valeur de ce paramètre
+    @Get('getWithParam/:id')
+    getWithParam(@Param('id') id: string): string {
+        return `Votre paramètre : ${id}.`;
+    }
+
+    // Route POST dotée d'un body qui renvoie la valeur de ce body
+    @Post('postWithBody')
+    postWithBody(@Body() body: string): string {
+        return `Le corps de la requête : ${JSON.stringify(body)}.`;
+    }
 }
